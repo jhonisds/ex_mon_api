293924aacdc11b4151ed829ca5af5a2d5149e3d9
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-09-04 18:55:41 -0300

    create route get trainer.

---
+++ lib/ex_mon_api/trainer/get.ex
@@ -0,0 +1,25 @@
+defmodule ExMonApi.Trainer.Get do
+  @moduledoc """
+  Module get trainers
+  """
+
+  alias ExMonApi.{Repo, Trainer}
+  alias Ecto.UUID
+
+  def call(id) do
+    case UUID.cast(id) do
+      :error -> {:error, "Invalid ID format"}
+      {:ok, uuid} -> get_trainer(uuid)
+    end
+  end
+
+  defp get_trainer(uuid) do
+    case Repo.get(Trainer, uuid) do
+      nil ->
+        {:error, "Trainer not found"}
+
+      trainer ->
+        {:ok, trainer}
+    end
+  end
+end
--- lib/ex_mon_api_web/views/trainer_view.ex
+++ lib/ex_mon_api_web/views/trainer_view.ex
@@ -13,4 +13,12 @@ defmodule ExMonApiWeb.TrainerView do
       }
     }
   end
+
+  def render("show.json", %{trainer: %Trainer{id: id, name: name, inserted_at: inserted_at}}) do
+    %{
+      id: id,
+      name: name,
+      inserted_at: inserted_at
+    }
+  end
 end
