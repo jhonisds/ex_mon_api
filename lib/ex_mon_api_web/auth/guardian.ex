7a741705111d45e15406715a2cc381cc820391cd
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-09-11 17:29:40 -0300

    add authenticate function.

--- lib/ex_mon_api_web/auth/guardian.ex
+++ lib/ex_mon_api_web/auth/guardian.ex
@@ -4,6 +4,8 @@ defmodule ExMonApiWeb.Auth.Guardian do
   """
   use Guardian, otp_app: :ex_mon_api
 
+  alias ExMonApi.{Repo, Trainer}
+
   def subject_for_token(trainer, _claims) do
     sub = to_string(trainer.id)
     {:ok, sub}
@@ -13,5 +15,24 @@ defmodule ExMonApiWeb.Auth.Guardian do
     claims
     |> Map.get("sub")
     |> ExMonApi.get_trainer()
+  end
+
+  def authenticate(%{"id" => trainer_id, "password" => password}) do
+    case Repo.get(Trainer, trainer_id) do
+      nil -> {:error, "Trainer not found!"}
+      trainer -> validate_password(trainer, password)
+    end
+  end
+
+  def validate_password(%Trainer{password_hash: hash} = trainer, password) do
+    case Argon2.verify_pass(password, hash) do
+      true -> create_token(trainer)
+      false -> {:error, :unauthorized}
+    end
+  end
+
+  def create_token(trainer) do
+    {:ok, token, _claims} = encode_and_sign(trainer)
+    {:ok, token}
   end
 end
