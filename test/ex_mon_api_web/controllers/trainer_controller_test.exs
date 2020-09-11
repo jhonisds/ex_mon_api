54a849a062a5b48ae97273710cf688e3dea172f0
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-09-11 18:29:54 -0300

    add token inside header.

--- test/ex_mon_api_web/controllers/trainer_controller_test.exs
+++ test/ex_mon_api_web/controllers/trainer_controller_test.exs
@@ -1,9 +1,22 @@
 defmodule ExMonApiWeb.Controller.TrainerControllerTest do
   use ExMonApiWeb.ConnCase
 
+  import ExMonApiWeb.Auth.Guardian
   alias ExMonApi.Trainer
 
   describe "show/2" do
+    setup %{conn: conn} do
+      params = %{name: "Jhoni", password: "123456"}
+      {:ok, trainer} = ExMonApi.create_trainer(params)
+      {:ok, token, _claims} = encode_and_sign(trainer)
+
+      header =
+        conn
+        |> put_req_header("authorization", "Bearer #{token}")
+
+      {:ok, conn: header}
+    end
+
     test "when there is a trainer with the given id, returns the trainer", %{conn: conn} do
       params = %{name: "Jhoni", password: "123456"}
 
