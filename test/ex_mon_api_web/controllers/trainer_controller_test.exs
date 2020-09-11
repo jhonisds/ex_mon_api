2fd654816ddd6972da53124ed4d26cf588e63cb2
Jhoni Santos <jhoni.sousa@fatec.sp.gov.br>
2020-09-11 15:14:44 -0300

    add test trainer controller show.

---
+++ test/ex_mon_api_web/controllers/trainer_controller_test.exs
@@ -0,0 +1,31 @@
+defmodule ExMonApiWeb.Controller.TrainerControllerTest do
+  use ExMonApiWeb.ConnCase
+
+  alias ExMonApi.Trainer
+
+  describe "show/2" do
+    test "when there is a trainer with the given id, returns the trainer", %{conn: conn} do
+      params = %{name: "Jhoni", password: "123456"}
+
+      {:ok, %Trainer{id: id}} = ExMonApi.create_trainer(params)
+
+      response =
+        conn
+        |> get(Routes.trainer_path(conn, :show, id))
+        |> json_response(:ok)
+
+      assert %{"id" => _id, "inserted_at" => _iserted_at, "name" => "Jhoni"} = response
+    end
+
+    test "when there is a error, returns the errors ", %{conn: conn} do
+      response =
+        conn
+        |> get(Routes.trainer_path(conn, :show, "1234"))
+        |> json_response(:bad_request)
+
+      expected = %{"message" => "Invalid ID format!"}
+
+      assert expected == response
+    end
+  end
+end
